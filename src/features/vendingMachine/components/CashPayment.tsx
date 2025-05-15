06048b7b7e6556a8e8d2c3a1fddc67e5a449c488
jhlee <jhlee89898989@gmail.com>
2025-05-15 15:01:27 +0900

    Feat: cash validation feature added

--- src/features/vendingMachine/components/CashPayment.tsx
+++ src/features/vendingMachine/components/CashPayment.tsx
@@ -1,15 +1,24 @@
 import { Button, Card, CardBody } from '@heroui/react';
 import { useVendingMachineContext } from '../contexts/VendingMachineContextProvider';
 import SuperBigText from './SuperBigText';
+import formatToWon from '../utils/formatToWon';
 
-const cashNotes = [100, 500, 1000, 5000, 10000];
+const cashNotes = [10, 50, 100, 500, 1000, 5000, 10000, 50000];
 
 export default function CashPayment() {
 	const { state, dispatch } = useVendingMachineContext();
 
 	const onPressCashValue = (amount: number) => {
-		dispatch({ type: 'ADD_FUNDS', amount });
-		dispatch({ type: 'TRANSITION_STATE', nextState: 'selection' });
+		if (isValidCash(amount)) {
+			dispatch({ type: 'ADD_FUNDS', amount });
+			dispatch({ type: 'TRANSITION_STATE', nextState: 'selection' });
+		} else {
+			alert(
+				`${formatToWon(amount)} ${
+					isCoin(amount) ? '동전은' : '지폐는'
+				} 사용할 수 없습니다.`
+			);
+		}
 	};
 
 	const onPressReturnCash = () => {
@@ -17,10 +26,35 @@ export default function CashPayment() {
 		dispatch({ type: 'TRANSITION_STATE', nextState: 'idle' });
 	};
 
+	const isValidCash = (value: number) => {
+		switch (value) {
+			case 100:
+			case 500:
+			case 1000:
+			case 5000:
+			case 10000:
+				return true;
+			default:
+				return false;
+		}
+	};
+
+	const isCoin = (value: number) => {
+		switch (value) {
+			case 1000:
+			case 5000:
+			case 10000:
+			case 50000:
+				return false;
+			default:
+				return true;
+		}
+	};
+
 	return (
 		<Card>
 			<CardBody className='gap-2'>
-				<div className='flex gap-2'>
+				<div className='grid grid-cols-5 gap-2'>
 					{cashNotes.map((note) => (
 						<Button
 							key={note}
