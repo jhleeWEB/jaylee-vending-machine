cc9cc5d3130f684b1285011b94e9117e8e8b9a93
jhlee <jhlee89898989@gmail.com>
2025-05-15 11:32:06 +0900

    Cleanup: remove unused useState()

--- src/features/vendingMachine/contexts/VendingMachineContextProvider.tsx
+++ src/features/vendingMachine/contexts/VendingMachineContextProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useReducer, useState } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
 import vendingMachineReducer, {
 	type VendingMachineAction,
 	type VendingMachineState,
